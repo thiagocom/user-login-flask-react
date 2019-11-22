38e14b304b8675bf93f6b03dc609de462b83396f
ThiagoCom <thiagocom@github.com>
2019-11-21 22:00:32 -0200

    Server added

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+
+def create_app(config_class=None):
+
+	"Factory app"
+
+	app = Flask("app")
+
+	def index():
+		return "Welcome to development world"
+
+	app.add_url_rule("/", "index", index)
+
+	# Configuration
+	app.config.from_object(config_class)
+
+	return app
